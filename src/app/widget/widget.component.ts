2ae64af3ecfe22f6b7a69cebf85c8f9099614613
Cornelia Schulz <cschulz@fraedom.com>
2018-08-23 16:57:21 +1200

    widget component added

--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -9,12 +9,56 @@ export class WidgetComponent implements OnInit {
 
   widget = 'very widgety';
   colour = 'blue';
+  hero = 'Moe';
+  heroes = ["Eenie", "Meanie", "Miney", "Moe"];
+  selectedWidget;
+  widgets = [
+    {
+      id: 1,
+      name: 'Red Widget',
+      description: 'This is a red widget'
+    },
+    {
+      id: 2,
+      name: 'Orange Widget',
+      description: 'This is an orange widget'
+    },
+    {
+      id: 3,
+      name: 'Yellow Widget',
+      description: 'This is a yellow widget'
+    },
+    {
+      id: 4,
+      name: 'Green Widget',
+      description: 'This is a green widget'
+    },
+    {
+      id: 5,
+      name: 'Blue Widget',
+      description: 'This is a blue widget'
+    },
+    {
+      id: 6,
+      name: 'Indigo Widget',
+      description: 'This is a indigo widget'
+    },
+    {
+      id: 7,
+      name: 'Violet Widget',
+      description: 'This is a violet widget'
+    }
+  ]
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  selected(widget){
+    this.selectedWidget = widget;
+  }
+  
   echo(message) {
     console.log("Message:", message);
   }
