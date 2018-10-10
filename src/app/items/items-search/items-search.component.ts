e5980e0b8b4ba3fe66b03a7d5b5a1d62069ae9bd
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-10-10 15:10:33 +1300

    implemented search feature with observables

--- src/app/items/items-search/items-search.component.ts
+++ src/app/items/items-search/items-search.component.ts
@@ -1,8 +1,7 @@
 import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { ItemsService } from '../../shared'
-
+import { Observable, fromEvent } from 'rxjs'
 import { map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
-import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-items-search',
@@ -10,11 +9,22 @@ import { fromEvent } from 'rxjs';
   styleUrls: ['./items-search.component.css']
 })
 export class ItemsSearchComponent implements OnInit {
+  // this is a reference to #itemsSearch in the items-search.component.html file
   @ViewChild('itemsSearch') itemsSearch;
+  items;
 
   constructor(private itemsService: ItemsService) { }
 
   ngOnInit() {
+    const search = this.getNativeElement(this.itemsSearch);
+    const subscription = fromEvent(search, 'keyup')
+      .pipe(debounceTime(200)) // delays event
+      .pipe(map((event: any) => event.target.value))
+      .pipe(switchMap(query => this.itemsService.search(query)))
+      .subscribe((items) => {
+        this.items = items;
+        console.log('Query: ', items);
+      })
   }
 
   getNativeElement(element){
