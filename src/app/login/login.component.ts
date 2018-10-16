06224944d4e97fc3d48709d93c51379582e1c427
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-10-16 16:40:49 +1300

    updating reactive forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { resetApplicationState } from '@angular/core/src/render3/instructions';
 
 interface Login {
       username: string;
@@ -12,14 +13,27 @@ interface Login {
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-    login = new FormGroup({
-       username: new FormControl('Nancy', Validators.minLength(2)),
-       password: new FormControl()
-    });
-  constructor() { }
+    login: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.login = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  submit({value, valid}: {value: Login, valid: boolean}) {
+    console.log(value, valid);
+    this.reset();
+  }
+
+  reset() {
+    this.login.reset({
+      username: '',
+      password: ''
+    });
   }
 
 }
+
--- src/app/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+interface Subscriber {
+  name: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-newsletter',
